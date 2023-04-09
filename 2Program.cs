512bbd94305f90a84895ef88ef12d2f5d856e9a4
znako <127133685+Bagemich@users.noreply.github.com>
2023-04-09 11:55:52 +0500

    Add files via upload

---
+++ 2Program.cs
@@ -0,0 +1,18 @@
+﻿ Console.Write("Введите 2 числа: ");
+ int[] Numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+ Console.Write( GapNumberSum (Numbers[0], Numbers[1]));
+
+void GapNumberSum(int Number1, int Number2, int sum)
+{
+     if (Number1 > Number2) 
+     {
+         Console.WriteLine($"Сумма натуральных элементов в промежутке от M до N: {sum}"); 
+
+     }
+       GapNumberSum(Number1, Number2, sum);
+      + = = + (Number1++);
+
+}
+ GapNumberSum(Number1, Number2, 0);
+
+ 
