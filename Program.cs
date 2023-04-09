fca3282a4126e7fbb3cc521ccce70c18c7babbec
znako <127133685+Bagemich@users.noreply.github.com>
2023-04-09 11:55:24 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿string OutPutNumber(int Number)
+{
+    if(Number == 1)
+    return 1.ToString();
+    string Empty = String.Empty;
+    return Empty + OutPutNumber(Number - 1) + ", " + Number.ToString();
+}
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write(OutPutNumber(number));
